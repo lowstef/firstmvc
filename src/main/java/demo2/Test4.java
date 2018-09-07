dcbcce588694843230c51acb0bf8ed916c90b773
lowstef <8451355@qq.com>
2018-09-07 15:27:28 +0800

    new

---
+++ src/main/java/demo2/Test4.java
@@ -0,0 +1,35 @@
+/**
+ * @author lowstef
+ * @create 2018/9/7
+ * @since 1.0.0
+ */
+package demo2;
+
+import java.util.Scanner;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+        String[] split = s.split("");
+
+        for (int i = 0; i < split.length; i++) {
+            for (int j = 0; j < split.length; j++) {
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
