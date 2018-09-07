4bc002dc78e6d301fa3948b379111497d70e79ca
lowstef <8451355@qq.com>
2018-09-07 15:28:37 +0800

    new

--- src/main/java/demo2/Test4.java
+++ src/main/java/demo2/Test4.java
@@ -17,7 +17,7 @@ public class Test4 {
 
         for (int i = 0; i < split.length; i++) {
             for (int j = 0; j < split.length; j++) {
-
+                System.out.println(split[j]);
 
             }
 
